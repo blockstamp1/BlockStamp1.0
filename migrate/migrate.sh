dcf05c4498b6374edc457b7ef37f4a67f5f232fa
blockstamp1 <root@vps.server.local>
2022-09-15 09:52:51 +0000

    checking api

--- migrate/migrate.sh
+++ migrate/migrate.sh
@@ -1,1 +1,1 @@
-cat export.csv | while read a; do curl -G localhost:3003/api/internal/migrate --data-urlencode "d=$a"; done
+cat export.csv | while read a; do curl -G localhost:3004/api/internal/migrate --data-urlencode "d=$a"; done
